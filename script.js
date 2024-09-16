6ca17b3aa18b49228d7c0f4bf274f77033435e72
NicLam1 <nicholaslam.2023@scis.smu.edu.sg>
2024-09-16 14:51:18 +0800

    update

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-let stars = document.getElementById('stars')
-let moon = document.getElementById('moon')
-let behind = document.getElementById('behind')
-let front = document.getElementById('front')
-let text = document.getElementById('textBanner')
-let btn = document.getElementById('exploreBtn')
+let stars = document.getElementById("stars");
+let moon = document.getElementById("moon");
+let behind = document.getElementById("behind");
+let front = document.getElementById("front");
+let text = document.getElementById("textBanner");
+let btn = document.getElementById("exploreBtn");
 
-window.addEventListener('scroll',function(){
-    let value = window.scrollY;
-    stars.style.left = -20 + (value * -0.02) + '%'
-    moon.style.top = value * 0.4 + 'px'
-    moon.style.marginLeft = value * 0.1 + 'px'
-    moon.style.rotate = value *0.02+ 'deg'
-    behind.style.top = value * 0.2 + 'px'
-    front.style.top = value * 0.05 + 'px'
-    text.style.marginRight = value * 0.7 + 'px'
-    text.style.marginTop = value * 1 + 'px'
-    btn.style.marginTop = 30 + value + 'px'
-    btn.style.marginLeft = value * 0.7 + 'px'
-})
-
-
-
-
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  stars.style.left = -20 + value * -0.02 + "%";
+  moon.style.top = value * 0.4 + "px";
+  moon.style.marginLeft = value * 0.1 + "px";
+  moon.style.rotate = value * 0.02 + "deg";
+  behind.style.top = value * 0.2 + "px";
+  front.style.top = value * 0.05 + "px";
+  text.style.marginRight = value * 0.7 + "px";
+  text.style.marginTop = value * 1 + "px";
+  btn.style.marginTop = 5 + value + "%";
+  btn.style.marginLeft = value * 0.7 + "px";
+});
