c0771e6547aa9ed0dd5258b51005969238d6d44d
NicLam1 <nicholaslam.2023@scis.smu.edu.sg>
2025-05-11 21:51:23 +0800

    resume change

--- src/Resume.js
+++ src/Resume.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './resume.css'; // Import the CSS file
-import resumeImage from './images/resume/NicholasLam_Resume.png'; // Ensure this path is correct
-import resumePDF from './images/resume/NicholasLam_Resume.pdf';
+import resumeImage1 from './images/resume/Nicholas_Lam_Resume_Page_1.jpg'; // Ensure this path is correct
+import resumeImage2 from './images/resume/Nicholas_Lam_Resume_Page_2.jpg'; // Ensure this path is correct
+import resumePDF from './images/resume/Nicholas_Lam_Resume.pdf';
 
 const Resume = () => {
   return (
     <section id="resume" className="resume-section">
-      <div className="container w-lg-50">
+      <div className="container">
       <h2 className="section-title">Resume</h2>
       <div className="resume-content">
-        <img src={resumeImage} alt="Resume" className="resume-image img-fluid" />
-        <a href={resumePDF} download="NicholasLam_Resume.pdf" className="download-button">
+        <img src={resumeImage1} alt="Resume" className="resume-image img-fluid" />
+        <img src={resumeImage2} alt="Resume" className="resume-image img-fluid" />
+        <a href={resumePDF} download="Nicholas_Lam_Resume.pdf" className="download-button">
           Download
         </a>
       </div>
